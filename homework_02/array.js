4596e0a638cb4e1a35d7e9581c6fc680209dbe9a
Surafel Nigussie Asfaw <surafel.nigussie12@gmail.com>
2019-04-02 23:57:27 -0500

    home work personal part

--- homework_02/array.js
+++ homework_02/array.js
@@ -1,21 +1,23 @@
 Array.prototype.even = function () {
     const array = this;
     return new Promise(function (resolve, reject) {
-        resolve(array.filter(e => e % 2 == 0));
+        resolve(array.filter(e => e % 2 === 0));
     })
-}
+};
 
 Array.prototype.odd = function () {
     const array = this;
     return new Promise(function (resolve, reject) {
-        resolve(array.filter(e => e % 2 == 1));
+        resolve(array.filter(e => e % 2 === 1));
     })
-}
+};
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log('start');
 array.even()
-    .then(e => console.log('even: ' + e));
+    .then(e => console.log('even: ' + e))
+    .catch(console.log);
 array.odd()
-    .then(e => console.log('odd: ' + e));
+    .then(e => console.log('odd: ' + e))
+    .catch(console.log);
 console.log('finish');
