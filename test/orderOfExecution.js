65e1e7c1351b76116ba5517e3041a1d7d8fa1576
surafel_nigussie <sasfaw@mum.edu>
2019-04-04 09:55:18 -0500

    test

---
+++ test/orderOfExecution.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(path.join(), 'utf8', function(){
+    setTimeout(() => {console.log('timeout')}, 0);
+    setImmediate(() => console.log('immediate'));
+    process.nextTick(() => console.log('nexttick'));
+})
+
+//tick is already at the IO phase after loading the file
