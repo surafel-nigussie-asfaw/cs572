ffb056dcb9c5424a469c672b1377dcffb2073eeb
Surafel Nigussie Asfaw <surafel.nigussie12@gmail.com>
2019-04-05 22:42:39 -0500

    home work 5

--- homework_05/app.js
+++ homework_05/app.js
@@ -1,29 +1,36 @@
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
 
-const app = express()
+const app = express();
 
-app.set('x-powered-by', false)
+app.set('x-powered-by', false);//mention framework
 
-app.set('strict routing', true)
+app.set('strict routing', true);//trailing slashes
 
-app.enable('case sensitive routing')
+app.enable('trust proxy');//proxy forwarding
+
+app.enable('case sensitive routing');//capital letter and small letters different
 
 app.get('/', (req, res) => {
-    res.send('welcome')
+    res.send('welcome');
     res.end()
-})
+});
 
 app.get('/users', (req, res) => {
     getUsers()
         .then(response => {
-            res.write(JSON.stringify(response.data), "utf-8");
+            // res.setHeader('Last-Modified', new Date());
+            // res.setHeader('Cache-Control', 'private, max-age=86400');
+            // res.setHeader('If-Modified-Since', new Date());
+            // res.setHeader('Link', '<http://localhost:3000/users?p=1> rel="first"')
+            //res.send(JSON.stringify(response), "utf-8");
+            res.status(200).send(response)
             res.end()
         })
         .catch(error => {
             console.log(error);
         })
-})
+});
 
 async function getUsers() {
     try {
@@ -33,4 +40,4 @@ async function getUsers() {
     }
 }
 
-app.listen(3000, () => console.error('listening to 3000'))
+app.listen(3000, () => console.error('listening to 3000'));
